5974a480a8a45eee0a40d7d48e3aa93daea938ba
antailbaxt3r <arjunbajpaiwork@gmail.com>
2019-08-03 17:33:28 +0530

    Fixed Display and Fixed ItemStorage

--- app/src/main/java/com/example/pokemonappdocw/ItemStorage.java
+++ app/src/main/java/com/example/pokemonappdocw/ItemStorage.java
@@ -1,15 +1,23 @@
 package com.example.pokemonappdocw;
 
+import com.example.pokemonappdocw.R;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-import androidx.recyclerview.widget.RecyclerView;
+import androidx.cardview.widget.CardView;
+import androidx.core.content.ContextCompat;
 
+import android.app.Dialog;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.LinearLayout;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
-import com.facebook.shimmer.ShimmerFrameLayout;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -17,20 +25,13 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Objects;
 
 public class ItemStorage extends AppCompatActivity {
 
-    private ShimmerFrameLayout shimmerFrameLayout;
-    private LinearLayout noItemLayout;
-    private RecyclerView recyclerView;
-    private ItemRVAdapter adapter;
-
     private Toolbar toolbar;
-
-    private List<Item> itemList = new ArrayList<>();
-
+    private TextView potions, revives;
+    private CardView potionCard, reviveCard;
     private DatabaseReference itemReference;
     private FirebaseAuth firebaseAuth;
 
@@ -56,30 +57,30 @@ public class ItemStorage extends AppCompatActivity {
             });
         }
 
-        shimmerFrameLayout.startShimmer();
+        System.out.println(firebaseAuth.getUid());
 
-        itemList.clear();
         itemReference.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                for (DataSnapshot shot : dataSnapshot.getChildren()) {
-                    Item item = shot.getValue(Item.class);
-                    itemList.add(item);
-                }
+                if (dataSnapshot.exists()) {
+                    String potionCount = "", reviveCount = "";
+                    if (dataSnapshot.child("potions").exists())
+                        potionCount = dataSnapshot.child("potions").getValue().toString();
+                    if (dataSnapshot.child("revives").exists())
+                        reviveCount = dataSnapshot.child("revives").getValue().toString();
 
-                if (itemList.isEmpty()){
-                    noItemLayout.setVisibility(View.VISIBLE);
-                    recyclerView.setVisibility(View.GONE);
-                }else {
-                    adapter = new ItemRVAdapter(getApplicationContext(), itemList);
-                    recyclerView.setAdapter(adapter);
-                    adapter.notifyDataSetChanged();
-                    recyclerView.setVisibility(View.VISIBLE);
-                    noItemLayout.setVisibility(View.GONE);
-                }
+                    if (potionCount.isEmpty() || potionCount.equals("0")) {
+                        potions.setText(getString(R.string.x0));
+                    } else {
+                        potions.setText("x" + potionCount);
+                    }
 
-                shimmerFrameLayout.stopShimmer();
-                shimmerFrameLayout.setVisibility(View.GONE);
+                    if (reviveCount.isEmpty() || reviveCount.equals("0")) {
+                        revives.setText(getString(R.string.x0));
+                    } else {
+                        revives.setText("x" + reviveCount);
+                    }
+                }
             }
 
             @Override
@@ -88,18 +89,136 @@ public class ItemStorage extends AppCompatActivity {
             }
         });
 
+        potionCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final Dialog potionDialog = new Dialog(ItemStorage.this);
+                potionDialog.setContentView(R.layout.dialog_box);
+                potionDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                TextView heading =  potionDialog.findViewById(R.id.dialog_box_heading);
+                heading.setText(getString(R.string.potion));
+                TextView body = potionDialog.findViewById(R.id.dialog_box_body);
+                body.setText(getString(R.string.usepotion));
+                Button positiveButton = potionDialog.findViewById(R.id.dialog_box_positive_button);
+                positiveButton.setText(getString(R.string.yes));
+                positiveButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        usePotion();
+                        itemReference.child("potions").addListenerForSingleValueEvent(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                                int count = Integer.parseInt(dataSnapshot.getValue().toString());
+                                if(count != 0){
+                                    count--;
+                                    itemReference.child("potions").setValue(count);
+                                }
+                                else{
+                                    Toast.makeText(ItemStorage.this, "You don't have any potions :(", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+
+                            @Override
+                            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                            }
+                        });
+
+                        potionDialog.dismiss();
+
+                    }
+                });
+                Button negativeButton = potionDialog.findViewById(R.id.dialog_box_negative_button);
+                negativeButton.setText(getString(R.string.cancel));
+                negativeButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        potionDialog.dismiss();
+                    }
+                });
+
+                potionDialog.show();
+
+
+
+
+            }
+        });
+
+        reviveCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final Dialog potionDialog = new Dialog(ItemStorage.this);
+                potionDialog.setContentView(R.layout.dialog_box);
+                potionDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                TextView heading =  potionDialog.findViewById(R.id.dialog_box_heading);
+                heading.setText(getString(R.string.revive));
+                TextView body = potionDialog.findViewById(R.id.dialog_box_body);
+                body.setText(getString(R.string.useRevive));
+                Button positiveButton = potionDialog.findViewById(R.id.dialog_box_positive_button);
+                positiveButton.setText(getString(R.string.yes));
+                positiveButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        useRevive();
+                        itemReference.child("revives").addListenerForSingleValueEvent(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                                int count = Integer.parseInt(dataSnapshot.getValue().toString());
+
+                                if(count != 0){
+                                    count--;
+                                    itemReference.child("revives").setValue(count);
+                                }
+                                else{
+                                    Toast.makeText(ItemStorage.this, "You don't have any revives :(", Toast.LENGTH_SHORT).show();
+                                }
+
+                            }
+
+                            @Override
+                            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                            }
+                        });
+                        potionDialog.dismiss();
+                    }
+                });
+                Button negativeButton = potionDialog.findViewById(R.id.dialog_box_negative_button);
+                negativeButton.setText(getString(R.string.cancel));
+                negativeButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        potionDialog.dismiss();
+                    }
+                });
+
+                potionDialog.show();
+
+            }
+        });
+
+    }
+
+    private void useRevive() {
+        //code to use Revive
+    }
+
+    private void usePotion() {
+        //code to use Potion
     }
 
     private void attachId(){
 
-        shimmerFrameLayout = findViewById(R.id.shimmerContainer_item_storage);
-        noItemLayout = findViewById(R.id.noItemsLayout);
-        recyclerView = findViewById(R.id.item_storage_rv);
+        potions = findViewById(R.id.potionCount);
+        revives = findViewById(R.id.reviveCount);
+        potionCard = findViewById(R.id.potionCard);
+        reviveCard = findViewById(R.id.reviveCard);
         toolbar = findViewById(R.id.toolbar);
-
         toolbar.setNavigationIcon(getDrawable(R.drawable.ic_arrow_back_red_24dp));
         setSupportActionBar(toolbar);
 
-        itemReference = FirebaseDatabase.getInstance().getReference().child(firebaseAuth.getCurrentUser().getUid()).child("itemList");
+        itemReference = FirebaseDatabase.getInstance().getReference().child("Users").child(firebaseAuth.getCurrentUser().getUid())
+                .child("itemList");
     }
 }
